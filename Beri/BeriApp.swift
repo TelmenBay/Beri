473674a1aeb344c60d0d14dc71a5a3c8fb0f5503
TelmenBay <bayarakh@gmail.com>
2025-08-13 02:44:23 -0400

    Initial Commit

---
+++ Beri/BeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeriApp.swift
+//  Beri
+//
+//  Created by Telmen Bayarbaatar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
